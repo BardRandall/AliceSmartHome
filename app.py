ecb24bd1f63c114319efecd6c6b1d5dd1c910b7a
Sbrodov Egor <egor-ulstu@yandex.ru>
2019-04-28 23:35:07 +0400

    Fix bug

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import logging
 app = Flask(__name__)
 SMARTHOMESERVER_ERROR = 202
 DEVICE_NOT_FOUND = 203
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://AliceSmartHub:qwerty123@AliceSmartHub.mysql.pythonanywhere-services.com/AliceSmartHub$smarthub'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123@localhost/alice'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'asjdbasidgausg7di73q783fad7fde'
 db = SQLAlchemy(app)
